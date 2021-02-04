a9bf6b6000b3842ca9e0150b499800fb4ab575fd
RowandMohammad <rowandnajim@gmail.com>
2021-02-04 21:35:24 +0000

    BackToMenu button from waiterview  is now fully functioning

--- OaxacaCafe/src/cs2810/WaiterViewController.java
+++ OaxacaCafe/src/cs2810/WaiterViewController.java
@@ -1,10 +1,17 @@
 package cs2810;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
 public class WaiterViewController {
 
@@ -21,9 +28,12 @@ public class WaiterViewController {
     private Label UserLabel;
 
     @FXML
-    void BackToOrderingPressed(ActionEvent event) {
-      System.out.println("leg");
-
-    }
+    void BackToOrderingPressed(ActionEvent event) throws IOException {
+		Parent menuViewParent = FXMLLoader.load(getClass().getResource("/CustomerView.fxml"));
+		Scene menuViewScene = new Scene(menuViewParent, 800, 800);
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		window.setScene(menuViewScene);
+		window.show();
+	}
 
 }
