7a1956f70d7db1f6cff152e3436290925d138b34
RowandMohammad <rowandnajim@gmail.com>
2021-02-09 19:04:19 +0000

    Updated checkout view controller with no conflicts

--- OaxacaCafe/src/cs2810/CheckoutViewController.java
+++ OaxacaCafe/src/cs2810/CheckoutViewController.java
@@ -1,33 +1,39 @@
 package cs2810;
 
 import java.io.IOException;
-import javafx.application.Application;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
-public class CheckoutViewController extends Application {
+public class CheckoutViewController {
+
+  @FXML
+  private Button backToOrder;
+
+  @FXML
+  private ListView<String> OrderList;
 
   @FXML
   void changeScreenButtonPushed(ActionEvent event) throws IOException {
-    Parent menuViewParent = FXMLLoader.load(getClass().getResource("/CustomerView.fxml"));
-    Scene menuViewScene = new Scene(menuViewParent);
+    Stage stage = (Stage) backToOrder.getScene().getWindow();
 
-    // This line gets the Stage information
-    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-    window.setScene(menuViewScene);
-    window.show();
+    stage.close();
   }
 
-  @Override
-  public void start(Stage args) throws Exception {
-    // TODO Auto-generated method stub
+  public void populateCheckout(ArrayList<Menu_Item> basket, Float price) {
+    String order = "";
+    for (int i = 0; i < basket.size(); i++) {
+      order = order + basket.get(i).name + "  £" + basket.get(i).price + "\n";
+    }
+    price = BigDecimal.valueOf(price).setScale(2, BigDecimal.ROUND_HALF_UP).floatValue();
+    OrderList.getItems().add(order);
+    OrderList.getItems().add("Total Price: £" + price + "");
 
   }
+
 }
 
