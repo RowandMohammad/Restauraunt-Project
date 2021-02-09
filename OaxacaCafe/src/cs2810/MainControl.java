0c95d17381a199de708bf6b83e302024bbc03511
RowandMohammad <rowandnajim@gmail.com>
2021-02-09 19:19:29 +0000

    Fixed the window sizing of the Menu

--- OaxacaCafe/src/cs2810/MainControl.java
+++ OaxacaCafe/src/cs2810/MainControl.java
@@ -15,7 +15,7 @@ public class MainControl extends Application{
   @Override
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("/CustomerView.fxml"));
-    Scene scene = new Scene(root, 800, 800);
+    Scene scene = new Scene(root);
     primaryStage.setScene(scene);
     primaryStage.show();
 
