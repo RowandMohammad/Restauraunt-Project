4411dec0fe8dc1017e4264b23573707e9b1f1c47
kaiwww <75780088+kaiwww@users.noreply.github.com>
2021-02-11 19:00:13 +0000

    Create LoginMessage.java

---
+++ OaxacaCafe/src/LoginMessage.java
@@ -0,0 +1,14 @@
+package cs2810;
+
+import java.util.*;
+
+
+
+public class LoginMessage {
+	private static Map<String, Object> message = new HashMap<String, Object>();
+	
+	public static Map<String, Object> getMessage() {
+		return message;
+	}
+
+}
