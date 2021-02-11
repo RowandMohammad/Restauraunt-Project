bafcbd7e0c31e9aa0b0f720b9718d63a9b7afb2f
RowandMohammad <rowandnajim@gmail.com>
2021-02-11 16:32:02 +0000

    Added database initialisation to maincontrol class

--- OaxacaCafe/src/cs2810/MainControl.java
+++ OaxacaCafe/src/cs2810/MainControl.java
@@ -1,24 +1,29 @@
 package cs2810;
 
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MainControl extends Application{
-  
-  public static void main(String[] args) {
-    launch(args);
-  }
-  
-  @Override
-  public void start(Stage primaryStage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("/CustomerView.fxml"));
-    Scene scene = new Scene(root);
-    primaryStage.setScene(scene);
-    primaryStage.show();
+public class MainControl extends Application {
 
-  }
+	public static void main(String[] args) throws URISyntaxException, SQLException {
+		DatabaseInitialisation.main(args);
+		launch(args);
+		
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("/CustomerView.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+	}
 
 }
