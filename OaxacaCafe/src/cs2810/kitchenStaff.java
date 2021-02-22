59da35bd7b61a4ead0a38b2e248fd6699aa28240
ZHAC063 <ZHAC063@live.rhul.ac.uk>
2021-02-22 06:59:40 +0000

    Updated kitchenStaff and waiterStaff Classes
    Added empty alertWaiter method in kitchenStaff
    Added empty alert method in waiterStaff

--- OaxacaCafe/src/cs2810/kitchenStaff.java
+++ OaxacaCafe/src/cs2810/kitchenStaff.java
@@ -1,8 +1,14 @@
 package cs2810;
 
+import java.util.ArrayList;
+
 public class kitchenStaff {
   String user;
   String pass;
+
+  void alertWaiter(ArrayList<Menu_Item> order, ArrayList<waiterStaff> waiters, int tableNum) {
+    // alert waiter for order to be served to table
+  }
 
   kitchenStaff() {
     this.user = "user";
--- OaxacaCafe/src/cs2810/waiterStaff.java
+++ OaxacaCafe/src/cs2810/waiterStaff.java
@@ -1,8 +1,14 @@
 package cs2810;
 
+import java.util.ArrayList;
+
 public class waiterStaff {
   String user;
   String pass;
+
+  public void alert(ArrayList<Menu_Item> order, int tableNum) {
+    // show notification to this waiter with order and table number
+  }
 
   waiterStaff() {
     this.user = "user";
