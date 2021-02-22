884fddea8808616252b439a9c33c2602a9d0271e
ZHAC063 <ZHAC063@live.rhul.ac.uk>
2021-02-22 06:23:16 +0000

    Updated MainControl Added two ArrayLists in MainControl to hold the kitchenStaff and waiterStaff objects Added method to initialise the staff objects within the Arrays

--- OaxacaCafe/src/cs2810/MainControl.java
+++ OaxacaCafe/src/cs2810/MainControl.java
@@ -2,7 +2,7 @@ package cs2810;
 
 import java.net.URISyntaxException;
 import java.sql.SQLException;
-
+import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,28 +11,37 @@ import javafx.stage.Stage;
 
 
 
-public class MainControl extends Application{
-  
+public class MainControl extends Application {
+  static ArrayList<kitchenStaff> KS = new ArrayList<kitchenStaff>();
+  static ArrayList<waiterStaff> WS = new ArrayList<waiterStaff>();
+
+  public static void main(String[] args) throws URISyntaxException, SQLException {
+    StaffInitialization();
+    DatabaseInitialisation.main(args);
+    launch(args);
+  }
 
 
-	public static void main(String[] args) throws URISyntaxException, SQLException {
-		DatabaseInitialisation.main(args);
-		launch(args);
-	}
-		
 
-
-	@Override
+  @Override
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("/CustomerView.fxml"));
     Scene scene = new Scene(root);
-    scene.getStylesheets().add(getClass().getClassLoader().getResource("styling/style.css").toExternalForm());
+    scene.getStylesheets()
+        .add(getClass().getClassLoader().getResource("styling/style.css").toExternalForm());
     primaryStage.setScene(scene);
     primaryStage.show();
     primaryStage.setResizable(false);
-    
+  }
 
-	}
+  static void StaffInitialization() {
+    for (int x = 0; x < 10; x++) {
+      KS.add(new kitchenStaff());
+    }
+    for (int y = 0; y < 10; y++) {
+      WS.add(new waiterStaff());
+    }
+  }
 
 
 
