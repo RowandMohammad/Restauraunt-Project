959a0d85aae3c66a2bd694298d1c5fe49244e409
Adam Bahnasi <adamtoo@hotmail.co.uk>
2021-03-04 16:31:26 +0000

    Created new controller class for PaymentView

---
+++ OaxacaCafe/src/cs2810/PaymentViewController.java
@@ -0,0 +1,20 @@
+package cs2810;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class PaymentViewController {
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField cardNoField;
+
+    @FXML
+    private TextField expiryField;
+
+    @FXML
+    private TextField cvcField;
+
+}
