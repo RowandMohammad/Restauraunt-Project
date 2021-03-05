51d5b2eb624b8b7dfdc0400e1c876cb918ea9bee
Adam Bahnasi <adamtoo@hotmail.co.uk>
2021-03-05 19:35:18 +0000

    Created skeleton code to check if card details are valid

--- OaxacaCafe/src/cs2810/PaymentViewController.java
+++ OaxacaCafe/src/cs2810/PaymentViewController.java
@@ -1,5 +1,6 @@
 package cs2810;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -20,5 +21,39 @@ public class PaymentViewController {
 
     @FXML
     private Button orderButton;
+    
+    private void checkCardDetails() {
+      if (isValidName() && isValidCardNo() && isValidExpiry() && isValidCVC()) {
+        //Create alert box that mentions that the transaction was successful
+      }
+      else {
+        //Create alert box that mentions invalid card details
+      }
+    }
+    
+    private boolean isValidCVC() {
+      // TODO Auto-generated method stub
+      return false;
+    }
+
+    private boolean isValidExpiry() {
+      // TODO Auto-generated method stub
+      return false;
+    }
+
+    private boolean isValidCardNo() {
+      // TODO Auto-generated method stub
+      return false;
+    }
+
+    private boolean isValidName() {
+      // TODO Auto-generated method stub
+      return false;
+    }
+
+    @FXML
+    void purchaseOrder(ActionEvent event) {
+      checkCardDetails();
+    }
 
 }
