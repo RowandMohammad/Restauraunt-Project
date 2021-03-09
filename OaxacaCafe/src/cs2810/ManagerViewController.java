4648358fd43ab67709624c5af9bcdae1e329c87e
RowandMohammad <rowandnajim@gmail.com>
2021-03-09 23:36:46 +0000

    Completed basic gui for manager view as well as button to navigate back and forth.

--- OaxacaCafe/src/cs2810/ManagerViewController.java
+++ OaxacaCafe/src/cs2810/ManagerViewController.java
@@ -1,5 +1,32 @@
 package cs2810;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 public class ManagerViewController {
+    private ViewCustomerInterface parent;
+
+    @FXML
+    private Button BackToOrdering;
+
+    @FXML
+    private Label UserLabel;
+
+    @FXML
+    private Button viewEmployee;
+
+    @FXML
+    void BackToOrderingPressed(ActionEvent event) {
+    	((Stage) BackToOrdering.getScene().getWindow()).close();
+
+    }
+
+    @FXML
+    void handleViewEmployee(ActionEvent event) {
+
+    }
 
 }
