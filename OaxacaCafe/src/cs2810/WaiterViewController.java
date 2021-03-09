7f75a87ab81149aa2fdaa754d613e56c1db05bbf
kaiwww <75780088+kaiwww@users.noreply.github.com>
2021-03-09 20:26:46 +0000

    Update WaiterViewController.java

--- OaxacaCafe/src/cs2810/WaiterViewController.java
+++ OaxacaCafe/src/cs2810/WaiterViewController.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
